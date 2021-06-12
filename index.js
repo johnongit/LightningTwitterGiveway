be1c0b159beb11542b1725c3a31f62ae7e96ebf8
Séraphin <seraphin.ribot@foncia.fr>
2021-06-12 09:55:41 +0200

    divide big function into multiple smaller ones

--- index.js
+++ index.js
@@ -1,97 +1,100 @@
 const { resolve } = require("path");
 const { exit } = require("process");
-var fs = require('fs');
-const schedule = require('node-schedule')
+var fs = require("fs");
+const schedule = require("node-schedule");
 
+const lnbitconnect = require("./utils/lnbitsconnect.js");
+const convert = require("./utils/convertsvg.js");
+const twitter = require("./utils/twitter.js");
 
+const config = require("./config.js").lnbitsConfig;
 
-const lnbitconnect = require('./utils/lnbitsconnect.js');
-const convert = require('./utils/convertsvg.js');
-const twitter = require('./utils/twitter.js');
+const lnbits_remote = {
+  admin_key: config.admin_key,
+  host: config.host,
+};
 
-const config = require('./config.js').lnbitsConfig;
+const lnbits_lnurlw_params = {
+  min_withdrawable: config.min_withdrawable,
+  max_withdrawable: config.max_withdrawable,
+  uses: config.uses,
+  wait_time: config.wait_time,
+};
 
+async function uploadLnUrlImg() {
+  // get SVG qrcode and convert it to png
+  const svg = await lnbitconnect.getLnUrlWImg(lnurl, lnbits_remote);
 
+  // convert it and save it on a tmp file
+  await convert.convertSvg2Img(svg);
 
-const lnbits_remote={
-    "admin_key":config.admin_key,
-    "host":config.host
+  // upload it to twitter
+  const { media_id_string } = await twitter.uploadImg();
+
+  // delete tmp image file
+  fs.unlinkSync("lnurl.png");
+
+  // send tweet (start giveaway)
+  const tweet = await twitter.sendTweet(media_id_string);
+  console.log(`You've successfully tweeted this : ${tweet.text}`);
 }
-const lnbits_lnurlw_params={
-    "min_withdrawable":config.min_withdrawable,
-    "max_withdrawable":config.max_withdrawable,
-    "uses":config.uses,
-    "wait_time": config.wait_time
-}
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-async function runBot() {
-    try {
-        // Create lnurl
-        lnurl = await lnbitconnect.createLnUrlW(lnbits_remote, lnbits_lnurlw_params)
-        lnurl = JSON.parse(lnurl)
-        // get SVG qrcode and convert it to png
-        svg = await lnbitconnect.getLnUrlWImg(lnurl,lnbits_remote)
-        await convert.convertSvg2Img(svg)
 
-        // upload it to twitter
-        let imgupld = await twitter.uploadImg()
-        let media_id_string = imgupld.media_id_string
+function checkEndGiveaway() {
+  // each 10 seconds, cehck giveaway status
+  const interval = setInterval(async () => {
+    const { used: updatedUsed } = await lnbitconnect.getLnUrlW(
+      lnurl,
+      lnbits_remote
+    );
 
-        // delete image file
-        fs.unlinkSync('lnurl.png')
-        
-        // send tweet (start giveaway)
-        let tweet = await twitter.sendTweet(media_id_string)
-        console.log('You successfully tweeted this : "' + tweet.text + '"');
-        
-        // check giveaway usage
-        lnurllink = JSON.parse(await lnbitconnect.getLnUrlW(lnurl, lnbits_remote))
-        used = lnurllink.used
-        uses = lnurllink.uses
-        
-        //poll lnurl usage
-        while(used<config.uses) {
-            await sleep(10000);
-            used = JSON.parse(await lnbitconnect.getLnUrlW(lnurl, lnbits_remote)).used
-        }
-
-        // giveaway empty, delete lnurl and close it on twitter
-        console.log("Giveway closed")
-        lnbitconnect.delLnUrlW(lnurl, lnbits_remote)
-        .then(() => { 
-            console.log("LNURL deleted")
-        })
-        .catch ((err) => {
-            console.log("Cannot delete LNURL")
-        })
-        
-        twitter.replyTweet(tweet.id_str)
-        .then((tweet) => {
-            console.log("Le tweet de de fermeture a été envoyé ", tweet.txt)
-            console.log("Close tweet sent ", tweet.txt)
-        })
-        .catch ((err) => {
-            console.log("Cannot send close tweet")
-        })
-
-
-
-    } catch (err) {
-        console.log("Error", err)
+    if (updatedUsed >= config.uses) {
+      await endGiveaway();
+      clearInterval(interval);
     }
+  }, 10000);
 }
 
-lnbitconnect.getWallet(lnbits_remote)
-.then(function (wallet) {
-    wallet = JSON.parse(wallet)
-    console.log("Connected to wallet: ", wallet.name)
-    runBot()
-    .catch((err) => console.log("Error ", err))
-})
-.catch((err) => {
+async function endGiveaway() {
+  console.log("Giveway ened.");
+  try {
+    await lnbitconnect.delLnUrlW(lnurl, lnbits_remote);
+    console.log("LNURL deleted");
+  } catch (err) {
+    console.log("An error occured while trying to delete LNURL", err);
+  }
+
+  try {
+    let close = await twitter.replyTweet(tweet.id_str);
+    console.log(`Giveaway closing tweet has been sent : ${close.txt}`);
+  } catch (err) {
+    console.log(
+      "An error occured while trying to send giveaway closing tweet",
+      err
+    );
+  }
+}
+
+async function runBot() {
+  // Create lnurl
+  const lnurl = await lnbitconnect.createLnUrlW(
+    lnbits_remote,
+    lnbits_lnurlw_params
+  );
+
+  await uploadLnUrlImg();
+
+  checkEndGiveaway();
+}
+
+lnbitconnect
+  .getWallet(lnbits_remote)
+  .then(async function (wallet) {
+    console.log("Connected to wallet: ", wallet.name);
+
+    try {
+      await runBot();
+    } catch (err) {}
+  })
+  .catch((err) => {
     console.log("Cannot connect to lnbits wallets");
-})
+  });
--- utils/lnbitsconnect.js
+++ utils/lnbitsconnect.js
@@ -1,174 +1,164 @@
-
 const https = require("https");
 
-
-
-
 function getWallet(lnbits_remote) {
-    const options = {
-        host: lnbits_remote.host,
-        port: 443,
-        path: '/api/v1/wallet',
-        method: 'GET',
-        headers: {
-            "X-Api-Key": lnbits_remote.admin_key
-        }
-    };
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, function(res) {
-            if(res.statusCode == 200) {
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    resolve(chunk)
-                });
-            } else {
-                reject(res)
-            }
+  const options = {
+    host: lnbits_remote.host,
+    port: 443,
+    path: "/api/v1/wallet",
+    method: "GET",
+    headers: {
+      "X-Api-Key": lnbits_remote.admin_key,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    const req = https.request(options, function (res) {
+      if (res.statusCode == 200) {
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) {
+          resolve(JSON.parse(chunk));
         });
-        req.on('error', (e) => {
-
-            reject(e)
-        });
-        req.end();
+      } else {
+        reject(res);
+      }
     });
-}
-
-function createLnUrlW(lnbits_remote,lnbits_lnurlw_params) {
-    const data = JSON.stringify({
-        title: "LnURL Giveaway",
-        min_withdrawable: lnbits_lnurlw_params.min_withdrawable,
-        max_withdrawable: lnbits_lnurlw_params.max_withdrawable,
-        uses: lnbits_lnurlw_params.uses,
-        wait_time: lnbits_lnurlw_params.wait_time,
-        is_unique:false
-    })
-    const options = {
-        host: lnbits_remote.host,
-        port: 443,
-        path: '/withdraw/api/v1/links',
-        method: 'POST',
-        headers: {
-            "X-Api-Key": lnbits_remote.admin_key,
-            'Content-Length': data.length,
-            'Content-Type': 'application/json',
-        }
-    };
-    
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, function(res) {
-            if(res.statusCode == 201) {
-                res.setEncoding('utf8');
-                res.on('data', d => {
-                    resolve(d)
-                })
-            }
-            else {
-                reject("Connot create LNURL, status code " + res.statCode);
-            }
-        });
-        req.on('error', err => {
-            console.log("err")
-            reject(err);
-            
-        })
-        
-        req.write(data)
-        req.end()
+    req.on("error", (e) => {
+      reject(e);
     });
+    req.end();
+  });
 }
 
-function getLnUrlWImg(params,lnbits_remote) {
-    const options = {
-        host: lnbits_remote.host,
-        port: 443,
-        path: '/withdraw/img/' + params.id,
-        method: 'GET',
-        headers: {
-            "X-Api-Key": lnbits_remote.admin_key
-        }
-    };
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, function(res) {
+function createLnUrlW(lnbits_remote, lnbits_lnurlw_params) {
+  const data = JSON.stringify({
+    title: "LnURL Giveaway",
+    min_withdrawable: lnbits_lnurlw_params.min_withdrawable,
+    max_withdrawable: lnbits_lnurlw_params.max_withdrawable,
+    uses: lnbits_lnurlw_params.uses,
+    wait_time: lnbits_lnurlw_params.wait_time,
+    is_unique: false,
+  });
+  const options = {
+    host: lnbits_remote.host,
+    port: 443,
+    path: "/withdraw/api/v1/links",
+    method: "POST",
+    headers: {
+      "X-Api-Key": lnbits_remote.admin_key,
+      "Content-Length": data.length,
+      "Content-Type": "application/json",
+    },
+  };
 
-            if(res.statusCode == 200) {
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    resolve(chunk)
-                });
-            } else {
-                reject(res)
-            }
+  return new Promise(function (resolve, reject) {
+    const req = https.request(options, function (res) {
+      if (res.statusCode == 201) {
+        res.setEncoding("utf8");
+        res.on("data", (d) => {
+          resolve(JSON.parse(d));
         });
-        req.on('error', (e) => {
-            reject(e)
-        });
-        req.end();
-    })
+      } else {
+        reject("Connot create LNURL, status code " + res.statCode);
+      }
+    });
+    req.on("error", (err) => {
+      console.log("err");
+      reject(err);
+    });
+
+    req.write(data);
+    req.end();
+  });
 }
 
-function getLnUrlW(params,lnbits_remote) {
-    const options = {
-        host: lnbits_remote.host,
-        port: 443,
-        path: '/withdraw/api/v1/links/' + params.id,
-        method: 'GET',
-        headers: {
-            "X-Api-Key": lnbits_remote.admin_key
-        }
-    };
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, function(res) {
-
-            if(res.statusCode == 200) {
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    resolve(chunk)
-                });
-            } else {
-                reject(res)
-            }
+function getLnUrlWImg(params, lnbits_remote) {
+  const options = {
+    host: lnbits_remote.host,
+    port: 443,
+    path: "/withdraw/img/" + params.id,
+    method: "GET",
+    headers: {
+      "X-Api-Key": lnbits_remote.admin_key,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    const req = https.request(options, function (res) {
+      if (res.statusCode == 200) {
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) {
+          resolve(chunk);
         });
-        req.on('error', (e) => {
-            reject(e)
-        });
-        req.end();
-    })
+      } else {
+        reject(res);
+      }
+    });
+    req.on("error", (e) => {
+      reject(e);
+    });
+    req.end();
+  });
 }
 
-function delLnUrlW(params,lnbits_remote) {
-    const options = {
-        host: lnbits_remote.host,
-        port: 443,
-        path: '/withdraw/api/v1/links/' + 1,
-        method: 'DELETE',
-        headers: {
-            "X-Api-Key": lnbits_remote.admin_key
-        }
-    };
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, function(res) {
-
-            if(res.statusCode == 204) {
-                res.setEncoding('utf8');
-                res.on('data', function (chunk) {
-                    resolve(chunk)
-                });
-            } else {
-                reject(res)
-            }
+function getLnUrlW(params, lnbits_remote) {
+  const options = {
+    host: lnbits_remote.host,
+    port: 443,
+    path: "/withdraw/api/v1/links/" + params.id,
+    method: "GET",
+    headers: {
+      "X-Api-Key": lnbits_remote.admin_key,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    const req = https.request(options, function (res) {
+      if (res.statusCode == 200) {
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) {
+          resolve(JSON.parse(chunk));
         });
-        req.on('error', (e) => {
-            reject(e)
-        });
-        req.end();
-    })
+      } else {
+        reject(res);
+      }
+    });
+    req.on("error", (e) => {
+      reject(e);
+    });
+    req.end();
+  });
 }
 
+function delLnUrlW(params, lnbits_remote) {
+  const options = {
+    host: lnbits_remote.host,
+    port: 443,
+    path: "/withdraw/api/v1/links/" + 1,
+    method: "DELETE",
+    headers: {
+      "X-Api-Key": lnbits_remote.admin_key,
+    },
+  };
+
+  return new Promise(function (resolve, reject) {
+    const req = https.request(options, function (res) {
+      if (res.statusCode == 204) {
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) {
+          resolve(chunk);
+        });
+      } else {
+        reject(res);
+      }
+    });
+    req.on("error", (e) => {
+      reject(e);
+    });
+    req.end();
+  });
+}
 
 module.exports = {
-    getWallet,
-    createLnUrlW,
-    getLnUrlWImg,
-    getLnUrlW,
-    delLnUrlW,
-}
+  getWallet,
+  createLnUrlW,
+  getLnUrlWImg,
+  getLnUrlW,
+  delLnUrlW,
+};
